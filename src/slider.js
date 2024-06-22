015d936c7b78bbc17d986305d47a5dddab8f9e6d
tediko <tediko123@wp.pl>
2024-06-22 12:13:11 +0200

    Added slider

---
+++ src/slider.js
@@ -0,0 +1,38 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+/**
+ * Initialize a Swiper slider instance.
+ * @param {string} containerSelector - The CSS selector for the slider container.
+ * @param {Object} [options={}] - Custom options to override default Swiper settings.
+ * @returns {Swiper|null} The Swiper instance or null if initialization fails.
+ */
+const initSlider = (containerSelector, options = {}) => {
+    const sliderContainer = document.querySelector(containerSelector);
+
+    if (!sliderContainer) {
+        console.error(`Slider container not found: ${sliderContainer}`);
+        return null;
+    }
+
+    try {
+        // default Swiper options
+        const defaultOptions = {
+            loop: false,
+            slidesPerView: "auto",
+            spaceBetween: 24,
+            speed: 600,
+            centeredSlides: false,
+        }
+        // Merge default options with custom options
+        const sliderOptions = { ...defaultOptions, ...options };
+
+        // Initialize and return the Swiper instance
+        return new Swiper(sliderContainer, sliderOptions);
+    } catch(error) {
+        console.error(`Error initializing slider: ${error}`);
+        return null;
+    }
+};
+
+export default initSlider;
