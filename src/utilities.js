4f4b604bb6770a3df989950f95e754d408ea4151
tediko <tediko123@wp.pl>
2024-06-27 14:18:16 +0200

    Added emailValidation, passwordValidation, showFormErrorMessage

--- src/utilities.js
+++ src/utilities.js
@@ -47,4 +47,62 @@ const focusTrap = (container) => {
     })
 }
 
-export { createHtmlElement, focusTrap };
+/**
+ * Validates an email address using a regular expression.
+ * @param {string} email - The email address to validate.
+ * @returns {Boolean} - Returns true if the email is valid or false otherwise.
+ */
+const emailValidation = (email) => {
+    const emailRegex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+    return emailRegex.test(email);
+}
+
+/**
+ * Validates a password using a regular expression.
+ * @param {string} password - The password to validate.
+ * @returns {Boolean} - Returns true if the password is valid or false otherwise.
+ */
+const passwordValidation = (password) => {
+    let requiredPasswordLength = 6;
+    return password.length >= requiredPasswordLength;
+}
+
+/**
+ * Displays error messages in a form and sets up auto-hiding on user input.
+ * @param {HTMLElement} formContainer - The container element of the form.
+ * @param {string[]} messages - An array of error messages to display.
+ */
+const showFormErrorMessage = (formContainer, messages) => {
+    const errorElement = formContainer.querySelector('[data-form-error]');
+
+    // Early return if inputs are invalid or error element doesn't exist
+    if (!formContainer || messages.length === 0 || !Array.isArray(messages)) return;
+    if (!errorElement) return;
+
+    // Flags
+    const activeClass = 'active';
+    const errorIcon = '<i class="fa-solid fa-circle-exclamation" aria-hidden="true"></i>';
+
+    // Create HTML string for error messages
+    const errorMsg = messages
+        .map(msg => `<span>${errorIcon} ${msg}</span>`)
+        .join('');
+
+    errorElement.innerHTML = errorMsg;
+    errorElement.classList.add(activeClass);
+    errorElement.setAttribute('aria-hidden', 'false');
+    
+    // Add a one-time event listener to hide the error when user starts typing
+    formContainer.addEventListener('input', () => {
+        errorElement.classList.remove(activeClass);
+        errorElement.setAttribute('aria-hidden', 'true');
+    }, { once: true})
+}
+
+export { 
+    createHtmlElement,
+    focusTrap,
+    emailValidation,
+    passwordValidation,
+    showFormErrorMessage
+};
