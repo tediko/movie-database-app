a61b089ed8a588233a1c0904c8c9d43f98d13eb7
tediko <tediko123@wp.pl>
2024-06-27 18:06:03 +0200

    Added registerFormValidation

---
+++ src/auth/registerFormValidation.js
@@ -0,0 +1,78 @@
+import { signUp } from "./authentication";
+import { emailValidation, passwordValidation, showFormErrorMessage, showRedirectSuccessMessage, redirectToNewLocation } from "../utilities";
+
+// Flags
+const loginPageUrl = '/access/login.html';
+const registerFormSelector = '[data-access-signup]';
+const registerContainerSelector = '[data-access-container]';
+
+/**
+ * Handles form submission, performs validation and initiates sign-up process.
+ * @param {Event} event - The submit event object.
+ * @param {HTMLElement} registerFormContainer - The register form container element.
+ */
+const handleFormSubmit = (event, registerFormContainer) => {
+    event.preventDefault();
+
+    const emailInputElement = registerFormContainer.querySelector('input[type="email"]');
+    const passwordInputElement = registerFormContainer.querySelectorAll('input[type="password"]');
+    const emailValue = emailInputElement.value;
+    const passwordValue = passwordInputElement[0].value;
+    const secondPasswordValue = passwordInputElement[1].value;
+
+    if (!emailValidation(emailValue) && !passwordValidation(passwordValue) || !passwordValidation(secondPasswordValue)) {
+        showFormErrorMessage(registerFormContainer, ['Invalid email address', 'Incorrect password (min. 6 characters)'])
+        return;
+    }
+
+    if (!emailValidation(emailValue)) {
+        showFormErrorMessage(registerFormContainer, ['Invalid email address'])
+        return;
+    }
+
+    if (!passwordValidation(passwordValue) || !passwordValidation(secondPasswordValue)) {
+        showFormErrorMessage(registerFormContainer, ['Incorrect password (min. 6 characters)'])
+        return;
+    }
+
+    if (passwordValue !== secondPasswordValue) {
+        showFormErrorMessage(registerFormContainer, ['Passwords must be the same'])
+        return;
+    }
+
+    register(emailValue, passwordValue, registerFormContainer);
+}
+
+/**
+ * Creates a new user using email and password, manages the sign-up process, and handles the outcome.
+ * @async
+ * @param {string} email - User's email address.
+ * @param {string} password - User's password.
+ * @param {HTMLElement} registerFormContainer - Container element for the login form.
+ * @throws {Error} If the sign-up attempt fails, the error is caught and displayed in the form container.
+ */
+async function register(email, password, registerFormContainer) {
+    try {
+        await signUp(email, password);
+        showRedirectSuccessMessage(registerContainerSelector, `Congratulations! You've successfully signed up!`)
+        redirectToNewLocation(loginPageUrl);
+    } catch(error) {
+        // A little workaround since there are no error codes with sign-up supabase auth.
+        // Returns string after colon in error message (e.g. "AuthApiError: error message"); 
+        const errorMsg = error.message.split(':')[1];
+        showFormErrorMessage(registerFormContainer, [`${errorMsg}`]);
+    }
+}
+
+/**
+ * Initializes the register form validation by setting up the submit event listener.
+ */
+const initRegisterFormValidation = () => {
+    const registerFormElement = document.querySelector(registerFormSelector);
+
+    if (!registerFormElement) return;
+
+    registerFormElement.addEventListener('submit', (event) => handleFormSubmit(event, registerFormElement));
+}
+
+export { initRegisterFormValidation };
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import './sass/main.scss';
 import { fetchUpcomingMovies } from './fetchUpcomingMovies';
 import initGetStartedEmailValidation from './getStartedEmailValidation';
 import { initLoginFormValidation, handleLoginButtonClick } from './auth/loginFormValidation';
+import { initRegisterFormValidation } from './auth/registerFormValidation';
 
 fetchUpcomingMovies();
 initGetStartedEmailValidation('[data-home-signup]');
 initLoginFormValidation();
+initRegisterFormValidation();
 handleLoginButtonClick();
