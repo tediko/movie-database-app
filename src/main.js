0cd60653cefa1ca935facf8dad1a411d88d308f5
tediko <tediko123@wp.pl>
2024-06-30 15:53:21 +0200

    Added initTrailers and removed fetchUpcomingMovies

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import './sass/main.scss';
-import { fetchUpcomingMovies } from './fetchData';
 import initGetStartedEmailValidation from './getStartedEmailValidation';
 import { initLoginFormValidation, handleLoginButtonClick } from './auth/loginFormValidation';
 import { initRegisterFormValidation } from './auth/registerFormValidation';
 import initTrending from './displayTrending';
+import initTrailers from './displayTrailers';
 
-fetchUpcomingMovies();
 initGetStartedEmailValidation('[data-home-signup]');
 initLoginFormValidation();
 initRegisterFormValidation();
 handleLoginButtonClick();
 initTrending();
+initTrailers();
