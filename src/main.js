e3fd70c66b6ee3ed2fedf2f23f0b8aed81a74d75
tediko <tediko123@wp.pl>
2024-06-30 14:57:42 +0200

    Added displayTrending to fetch and display a list of trending movies or TVSeries

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { fetchUpcomingMovies } from './fetchData';
 import initGetStartedEmailValidation from './getStartedEmailValidation';
 import { initLoginFormValidation, handleLoginButtonClick } from './auth/loginFormValidation';
 import { initRegisterFormValidation } from './auth/registerFormValidation';
+import initTrending from './displayTrending';
 
 fetchUpcomingMovies();
 initGetStartedEmailValidation('[data-home-signup]');
 initLoginFormValidation();
 initRegisterFormValidation();
 handleLoginButtonClick();
+initTrending();
