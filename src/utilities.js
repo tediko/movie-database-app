c62bf5a2cdebcc8ccd5640159aa646920e7c6aee
tediko <tediko123@wp.pl>
2024-07-02 12:21:15 +0200

    Added createBookmarkHtmlElement

--- src/utilities.js
+++ src/utilities.js
@@ -154,6 +154,26 @@ const displayDataError = (listContainer) => {
     listContainer.appendChild(fragment);
 }
 
+/**
+ * Creates HTML string for a bookmark button element
+ * @param {Object} bookmarks - Object containing arrays of bookmarked items by type.
+ * @param {Object} bookmarkInfo - Object containing information about the item to be bookmarked.
+ * @returns {string} HTML string representing a bookmark button element.
+ */
+const createBookmarkHtmlElement = (bookmarks, bookmarkInfo) => {
+    const {id, title, type} = bookmarkInfo;
+
+    // Since bookmarks will return object like { movie: ['1', '2'], tv: ['1'] } and id's can be the same for both movie and tv series
+    // we need to return bookmarked items by media type to get correct one.
+    const bookmarkedItems = bookmarks[type];
+    // Checks if bookmarkedItems has movie/tv series id assigned to our bookmark cta.
+    const isBookmarked = bookmarkedItems.includes(id.toString());
+
+    return isBookmarked ? 
+        `<button class="media-showcase__bookmark-cta bookmark-cta text-white" type="button" aria-label="Remove ${title} from bookmarks" data-bookmark-cta data-bookmarked data-bookmark-info='${JSON.stringify(bookmarkInfo)}'></button>` : 
+        `<button class="media-showcase__bookmark-cta bookmark-cta text-white" type="button" aria-label="Add ${title} to bookmarks" data-bookmark-cta data-bookmark-info='${JSON.stringify(bookmarkInfo)}'></button>`;
+}
+
 export { 
     createHtmlElement,
     focusTrap,
@@ -162,5 +182,6 @@ export {
     showFormErrorMessage,
     showRedirectSuccessMessage,
     redirectToNewLocation,
-    displayDataError
+    displayDataError,
+    createBookmarkHtmlElement
 };
