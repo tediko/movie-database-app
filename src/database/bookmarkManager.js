b670e157cb5b51e1d8ebd0e57ce1ae078bec7691
tediko <tediko123@wp.pl>
2024-07-03 12:50:25 +0200

    Added bookmarkManager to manage the state of bookmarks across different components

---
+++ src/database/bookmarkManager.js
@@ -0,0 +1,56 @@
+import { getUserBookmarks, updateUserBookmarks } from ".";
+
+let bookmarks = [];
+
+/**
+ * Initializes the bookmarks by fetching them from the database.
+ * @async
+ */
+async function init() {
+    bookmarks = await getUserBookmarks();
+}
+
+/**
+ * Checks if a bookmark with the given id and type exists in the bookmarks array.
+ * @param {number} id - The id of the bookmark to check.
+ * @param {string} type - The type of the bookmark to check.
+ */
+const isBookmarked = (id, type) => {
+    return bookmarks.find(item => item.id === id && item.type === type) !== undefined;
+}
+
+/**
+ * Returns a deep copy of the bookmarks array.
+ * @returns {Array<Object>} - A new array containing the same elements as the bookmarks array.
+ */
+const getBookmarks = () => {
+    return [...bookmarks];
+}
+
+/**
+ * Toggles the bookmark state for the given bookmark.
+ * If the bookmark exists, it removes it from the current bookmarks array.
+ * If the bookmark doesn't exists, it adds it to the current bookmarks array.
+ * @async
+ * @param {Object} newBookmark - The bookmark object to toggle
+ */
+async function toggleBookmark(newBookmark) {
+    // Find the index of the bookmark in the bookmarks array, if it exists
+    const index = bookmarks.findIndex(item => item.id === newBookmark.id && item.type === newBookmark.type);
+
+    // If the bookmark exists (index !== -1), remove it from the array
+    // Otherwise, add the new bookmark to the array
+    if (index !== -1) {
+        bookmarks.splice(index, 1);
+    } else {
+        bookmarks.push(newBookmark);
+    }
+    await updateUserBookmarks(bookmarks);
+}
+
+export const bookmarkManager = {
+    init,
+    toggleBookmark,
+    isBookmarked,
+    getBookmarks
+}
