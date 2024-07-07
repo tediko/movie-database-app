e2e6050caf090d8c0ef2a2f29ee67ab5aa5db366
tediko <tediko123@wp.pl>
2024-07-07 11:21:08 +0200

    Added getUser function to check if user is logged in. If not, redirect from app to login page

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ import initRecommended from './view/displayRecommended';
 import initTopRated from './view/displayTopRated';
 import { bookmarkManager } from './database/bookmarkManager';
 import initBookmarks from './view/displayBookmarks';
-import { signOut } from './auth/authentication';
+import { getUser, signOut } from './auth/authentication';
 
 async function initApp() {
+    const user = await getUser();
     const location = window.location.href;
+    
     if (!location.includes('app')) return;
+    if (!user) {
+        window.location.href = '/access/login.html';
+    };
 
     await bookmarkManager.init();
     initRecommended();
