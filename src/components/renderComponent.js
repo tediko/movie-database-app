c479ae0700245da03808d1ba8ec34522b0478cf1
tediko <tediko123@wp.pl>
2024-07-09 14:20:55 +0200

    Added function to render component

---
+++ src/components/renderComponent.js
@@ -0,0 +1,74 @@
+import { createHtmlElement } from "../utilities";
+import { initBookmarks, getBookmarksHtml } from './bookmarks';
+import { initTrending, getTrendingHtml } from "./trending";
+import { initTrailers, getTrailersHtml } from "./trailers";
+import { initRecommended, getRecommendedHtml } from "./recommended";
+import { initTopRated, getTopRatedHtml } from "./topRated";
+
+// Selectors
+const rootSelector = `#root`;
+
+/**
+ * Renders a component in the root element.
+ * @param {string} componentName - The name of the component to render.
+ */
+const renderComponent = (componentName) => {
+    // Check if root element exists.
+    const root = document.querySelector(rootSelector);
+    if (!root) return;
+    
+    // Checks if the componentName key exists in the componentTemplates object.
+    if (!componentTemplates[componentName]) {
+        console.error(`Template for component "${componentName}" not found.`);
+        return;
+    };
+    
+    // Gets component HTML string
+    const componentTemplate = componentTemplates[componentName].html;
+    // Create a new section element with the specified tag and classes
+    const componentWrapper = createHtmlElement(componentTemplates[componentName].tag, componentTemplates[componentName].classes, '');
+    // Parse the HTML template string into a DocumentFragment
+    const fragment = document.createRange().createContextualFragment(componentTemplate);
+    
+    // Append DocumentFragment to the section element and insert the new view section into the root element.
+    componentWrapper.appendChild(fragment);
+    root.appendChild(componentWrapper);
+
+    // Initializes component
+    componentTemplates[componentName].init();
+}
+
+const componentTemplates = {
+    bookmarks: {
+        html: getBookmarksHtml(),
+        tag: 'section',
+        classes: ['app__media-showcase', 'media-showcase'],
+        init: initBookmarks
+    },
+    top: {
+        html: getTopRatedHtml(),
+        tag: 'section',
+        classes: ['app__media-showcase', 'media-showcase'],
+        init: initTopRated
+    },
+    trending: {
+        html: getTrendingHtml(),
+        tag: 'section',
+        classes: ['app__trending', 'trending'],
+        init: initTrending
+    },
+    trailers: {
+        html: getTrailersHtml(),
+        tag: 'section',
+        classes: ['app__trailers', 'trailers--small'],
+        init: initTrailers
+    },
+    recommended: {
+        html: getRecommendedHtml(),
+        tag: 'section',
+        classes: ['app__media-showcase', 'media-showcase'],
+        init: initRecommended
+    }
+}
+
+export default renderComponent;
