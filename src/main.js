c60d429d7dbf7fb09c04bcc9fc1c15e4f62127d2
tediko <tediko123@wp.pl>
2024-07-09 15:29:46 +0200

    Changed app.js import path

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import initGetStartedEmailValidation from './getStartedEmailValidation';
 import { initLoginFormValidation, handleLoginButtonClick } from './auth/loginFormValidation';
 import { initRegisterFormValidation } from './auth/registerFormValidation';
 import { initTrailers } from './components/trailers';
-import initApp from './app';
+import { initApp } from './app';
 
 initGetStartedEmailValidation('[data-home-signup]');
 initLoginFormValidation();
