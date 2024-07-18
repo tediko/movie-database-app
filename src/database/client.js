1a661b9d8f0b07e4d4fc7ff0996add22272c22bb
tediko <tediko123@wp.pl>
2024-07-18 16:55:59 +0200

    Added supabvaseServiceKey and pass it in headers for client to bypass Row Level Security

--- src/database/client.js
+++ src/database/client.js
@@ -2,7 +2,15 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseServiceKey = import.meta.env.VITE_SUPABASE_SERVICE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+    global: {
+        headers: {
+        apikey: supabaseServiceKey,
+        Authorization: `Bearer ${supabaseServiceKey}`
+        }
+    }
+})
 
 export default supabase;
