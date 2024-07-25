fd4f662b7fc709d1438cf47876dbd1534636ba76
tediko <tediko123@wp.pl>
2024-07-25 15:16:24 +0200

    Refactored handler function to determine HTTP method, process query params for GET and body for POST requests and handle action without boilerplate code

--- functions/database/index.js
+++ functions/database/index.js
@@ -2,76 +2,58 @@ import { getUserBookmarks, getGenres, getRandomMedia, downloadAvatar, updateUser
 
 /**
  * Handler function for processing HTTP requests in a Netlify function.
+ * Supports both GET and POST methods to perform various actions 
+ * based on the specified action parameter in the request.
  * @async
  * @param {Object} event - The event object containing details about the HTTP request.
  * @returns {Promise<Object>} A promise that resolves to an object containing the HTTP response.
  */
 const handler = async (event) => {
-    if (event.httpMethod === 'GET') {
-        const { action, ...params } = event.queryStringParameters || {};
+    // Determine the HTTP method of the request
+    const isHttpMethodGet = event.httpMethod === 'GET';
+    const isHttpMethodPost = event.httpMethod === 'POST';
+    let action, params, requestBody;
 
-        const actionsMap = {
-            getUserBookmarks: async () => await getUserBookmarks(params.userUid),
-            getGenres: async () => await getGenres(),
-            getRandomMedia: async () => await getRandomMedia(),
-            downloadAvatar: async () => await downloadAvatar(params.userUid)
-        }
-        
-        try {
-            const actionHandler = actionsMap[action];
+    // Process query parameters for GET and body for POST requests
+    if (isHttpMethodGet || isHttpMethodPost) {
+        ({ action, ...params } = event.queryStringParameters || {});
 
-            if (!actionHandler) {
-                return {
-                    statusCode: 400,
-                    body: JSON.stringify({error: 'Invalid action'})
-                }
-            }
-
-            const response = await actionHandler();
-            
-            return {
-                statusCode: 200,
-                body: JSON.stringify(response)
-            }
-        } catch (error) {
-            return {
-                statusCode: 500,
-                body: JSON.stringify(error.message)
-            }
+        if (isHttpMethodPost) {
+            requestBody = JSON.parse(event.body);
         }
     }
 
-    if (event.httpMethod === 'POST') {
-        const { action } = event.queryStringParameters || {};
-        const requestBody = JSON.parse(event.body);
+    // Map of actions to corresponding handler functions
+    const actionsMap = {
+        getUserBookmarks: async () => await getUserBookmarks(params.userUid),
+        getGenres: async () => await getGenres(),
+        getRandomMedia: async () => await getRandomMedia(),
+        downloadAvatar: async () => await downloadAvatar(params.userUid),
+        updateUserBookmarks: async () => await updateUserBookmarks(requestBody.updatedBookmarks, requestBody.userUid),
+        createRecord: async () => await createRecord(requestBody.userUid),
+        uploadAvatar: async () => await uploadAvatar(requestBody.avatarFileName, requestBody.base64)
+    }
 
-        const actionsMap = {
-            updateUserBookmarks: async () => await updateUserBookmarks(requestBody.updatedBookmarks, requestBody.userUid),
-            createRecord: async () => await createRecord(requestBody.userUid),
-            uploadAvatar: async () => await uploadAvatar(requestBody.avatarFileName, requestBody.base64)
+    try {
+        const actionHandler = actionsMap[action];
+
+        if (!actionHandler) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({error: 'Invalid action'})
+            }
         }
+
+        const response = await actionHandler();
         
-        try {
-            const actionHandler = actionsMap[action];
-
-            if (!actionHandler) {
-                return {
-                    statusCode: 400,
-                    body: JSON.stringify({error: 'Invalid action'})
-                }
-            }
-
-            const response = await actionHandler();
-            
-            return {
-                statusCode: 200,
-                body: JSON.stringify(response)
-            }
-        } catch (error) {
-            return {
-                statusCode: 500,
-                body: JSON.stringify(error.message)
-            }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response)
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error.message)
         }
     }
 }
