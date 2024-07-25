98707b9a100a9398009244b536d331623ceec5b5
tediko <tediko123@wp.pl>
2024-07-25 12:43:08 +0200

    Moved fetching from api to Netlify serverless functions, added endpoints within api/fetchData.js for serverless fetch and changed comments

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,251 +1,142 @@
 // Flags
-const apiMovieUrl = 'https://api.themoviedb.org/3/movie/';
-const apiTrendingUrl = 'https://api.themoviedb.org/3/trending/all/week';
-const apiSearchUrl = 'https://api.themoviedb.org/3/search/multi?query=';
-
-// Fetch options
-const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${import.meta.env.VITE_TMBD_API_KEY}`
-    }
-};
+const fetchDataEndpoint = `/.netlify/functions/api-fetchData`;
+const endpoints = {
+    fetchUpcomingMovies: `${fetchDataEndpoint}?action=fetchUpcomingMovies`,
+    fetchTrailerSrcKey: `${fetchDataEndpoint}?action=fetchTrailerSrcKey`,
+    fetchTrending: `${fetchDataEndpoint}?action=fetchTrending`,
+    fetchRecommendations: `${fetchDataEndpoint}?action=fetchRecommendations`,
+    fetchTopRated: `${fetchDataEndpoint}?action=fetchTopRated`,
+    fetchSearchResults: `${fetchDataEndpoint}?action=fetchSearchResults`,
+    fetchMediaDetails: `${fetchDataEndpoint}?action=fetchMediaDetails`
+}
 
 /**
- * Fetches upcoming movies from the API and displays them.
+ * Fetches upcoming movies from a Netlify serverless function.
  * @async
- * @throws {Error} If there's an issue with the API request or response parsing.
- * @returns {Promise<void>}
+ * @returns {Promise<Array<Object>>} A promise that resolves to an array of movie objects.
+ * @throws {Error} Throws an error if the fetch request fails or if the response is not ok.
  */
 async function fetchUpcomingMovies() {
     try {
-        const response = await fetch(`${apiMovieUrl}/upcoming?language=en-US&page=1`, options);
+        const response = await fetch(endpoints.fetchUpcomingMovies);
+        const data = await response.json();
 
         if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+            throw new Error(data);
         }
-
-        const data = await response.json();
-        const moviesList = data.results.map(movie => {
-            return { 
-                id: movie.id,
-                title: movie.title,
-                background: movie.backdrop_path,
-                releaseDate: movie.release_date 
-            }
-        });
-
-        return moviesList;
+        
+        return data;
     } catch (error) {
-        console.error(`Error fetching upcoming movies: ${error}`);
         throw error;
     }
 }
 
 /**
- * Fetches the trailer source key for a given movie ID.
+ * Fetches trailer source key for given movieId from a Netlify serverless function.
  * @async
- * @param {number} movieId - The unique identifier of the movie.
- * @throws {Error} If there's an issue with the API request, response parsing , or if no trailer is found.
- * @returns {Promise<string>} The key for the movie's trailer video.
+ * @param {string|number} movieId - The movie ID for trailer as a string or number
+ * @returns {Promise<Array<Object>>} A promise that resolves to an array of movie objects.
+ * @throws {Error} Throws an error if the fetch request fails or if the response is not ok.
  */
 async function fetchTrailerSrcKey(movieId) {
-    if (!movieId || typeof movieId !== 'number') {
-        throw new TypeError('Invalid movieId. Expected a number');
-    }
-
     try {
-        const response = await fetch(`${apiMovieUrl}${movieId}/videos`, options);
+        const response = await fetch(`${endpoints.fetchTrailerSrcKey}&movieId=${movieId}`);
+        const data = await response.json();
 
         if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+            throw new Error(data);
         }
 
-        const data = await response.json();
-        const trailerInfo = data.results.find(trailer => trailer.type === "Trailer");
-
-        if (!trailerInfo) {
-            throw new Error(`No trailer found for movieId: ${movieId}`);
-        }
-                
-        return trailerInfo.key;
+        return data;
     } catch (error) {
-        console.error(`Error fetching trailer src key: ${error}`);
         throw error;
     }
 }
 
 /**
- * Fetches the latest trending movies and TV series from the API.
+ * Fetches the latest trending movies and TV series from a Netlify serverless function.
  * @async
- * @throws {Error} If there's an issue with the API request or response parsing.
- * @returns {Object<Array>} An array of objects containing the trending movies and TV series data.
+ * @returns {Promise<Array<Object>>} A promise that resolves to an array of movies and TV series objects.
+ * @throws {Error} Throws an error if the fetch request fails or if the response is not ok.
  */
 async function fetchTrending() {
     try {
-        const response = await fetch(apiTrendingUrl, options);
+        const response = await fetch(endpoints.fetchTrending);
+        const data = await response.json();
 
         if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+            throw new Error(data);
         }
 
-        const data = await response.json();
-        const filteredData = data.results.filter((item) => {
-            return item.media_type == 'movie' || item.media_type == 'tv';
-        }).map((item) => {
-            return {
-                id: item.id,
-                title: item.title || item.name,
-                posterPath: item.poster_path,
-                backdropPath: item.backdrop_path,
-                type: item.media_type,
-                releaseData: item.release_date || item.first_air_date,
-                ratingAverage: item.vote_average,
-                genreIds: item.genre_ids
-            }
-        })
-
-        return filteredData;
+        return data;
     } catch (error) {
         throw error;
     }
 }
 
 /**
- * Fetches movie and TV series recommendations based on the provided IDs.
+ * Fetches movie and TV series recommendations based on the provided IDs from a Netlify serverless function.
  * @async
- * @param {number|string} movieId - The ID of the movie to fetch recommendations for.
- * @param {number|string} seriesId - The ID of the TV series to fetch recommendations for.
- * @throws {Error} If there's an issue with the API request or response parsing.
+ * @param {string|number} movieId - The ID of the movie to fetch recommendations for.
+ * @param {string|number} seriesId - The ID of the tv series to fetch recommendations for.
  * @returns {Promise<{movies: any[], tvSeries: any[]}>} A promise that resolves with an object containing movie and TVseries recommendations.
+ * @throws {Error} Throws an error if the fetch request fails or if the response is not ok.
  */
 async function fetchRecommendations(movieId, seriesId) {
-    const urls = [
-        `https://api.themoviedb.org/3/movie/${movieId}/recommendations`,
-        `https://api.themoviedb.org/3/tv/${seriesId}/recommendations`
-    ];
-
     try {
-        // Takes an iterable of promises and returns a single Promise.
-        const responses = await Promise.all(urls.map(url => fetch(url, options)));
+        const response = await fetch(`${endpoints.fetchRecommendations}&movieId=${movieId}&seriesId=${seriesId}`);
+        const data = await response.json();
 
-        responses.forEach(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP Error! Status: ${response.status}`)
-            }
-        })
-
-        const data = await Promise.all(responses.map(res => res.json()));
-
-        data.forEach(item => {
-            if (Array.isArray(item.results) && !item.results.length) {
-                throw new Error(`Data error: Data array is empty!`);
-            }
-        })
-    
-        return {
-            movies: data[0].results.map(item => {
-                return {
-                    id: item.id,
-                    title: item.title,
-                    backdropPath: item.backdrop_path,
-                    type: item.media_type,
-                    releaseData: item.release_date,
-                    genreIds: item.genre_ids
-                }
-            }),
-            tv_series: data[1].results.map(item => {
-                return {
-                    id: item.id,
-                    title: item.name,
-                    backdropPath: item.backdrop_path,
-                    type: item.media_type,
-                    releaseData: item.first_air_date,
-                    genreIds: item.genre_ids
-                }
-            })
+        if (!response.ok) {
+            throw new Error(data);
         }
+
+        return data;
     } catch (error) {
         throw error;
     }
 }
 
 /**
- * Fetches top-rated movies or TV shows from the API.
+ * Fetches top-rated movies or TV shows from a Netlify serverless function.
  * @async
  * @param {('movie'|'tv')} [type='movie'] - The type of content to fetch. Must be either 'movie' or 'tv'. Defaults to 'movie'
  * @param {number} [page=1] - The page number of results to fetch. Defaults to 1.
  * @returns {Promise<Array<Object>>} A promise that resolves to an array of filtered content objects.
- * @throws {Error} Throws an error if the HTTP request fails or if there's an issue with the fetch operation.
+ * @throws {Error} Throws an error if the fetch request fails or if the response is not ok.
  */
 async function fetchTopRated(type = 'movie', page = 1) {
-    const url = `https://api.themoviedb.org/3/${type}/top_rated?language=en-US&page=${page}`;
-
     try {
-        const response = await fetch(url, options);
+        const response = await fetch(`${endpoints.fetchTopRated}&type=${type}&page=${page}`);
+        const data = await response.json();
 
         if (!response.ok) {
-            throw new Error(`HTTP Error! Status: ${response.status}`);
+            throw new Error(data);
         }
 
-        const data = await response.json();
-        const filteredData = data.results.map((item) => {
-            return {
-                id: item.id,
-                title: item.title || item.name,
-                backdropPath: item.backdrop_path,
-                type: type,
-                releaseData: item.release_date || item.first_air_date,
-                ratingAverage: item.vote_average,
-                genreIds: item.genre_ids
-            }
-        })
-
-        return filteredData;
+        return data;
     } catch (error) {
         throw error;
     }
 }
 
 /**
- * Fetches search results from an API based on the given search query.
+ * Fetches search results based on the given search query from a Netlify serverless function.
  * @async
  * @param {string} searchQuery - The search query to be used for fetching results.
  * @returns {Promise<Array<Object>>} - A promise that resolves to an array of filtered and formatted search results.
- * @throws {Error} Throws an error if the API request fails or if there's an HTTP error.
+ * @throws {Error} Throws an error if the fetch request fails or if the response is not ok.
  */
 async function fetchSearchResults(searchQuery) {
-    const url = `${apiSearchUrl}${searchQuery}&include_adult=false&language=en-US&page=1`
-
     try {
-        const response = await fetch(url, options);
+        const response = await fetch(`${endpoints.fetchSearchResults}&searchQuery=${searchQuery}`);
+        const data = await response.json();
 
         if (!response.ok) {
-            throw new Error(`HTTP Error! Status: ${response.status}`);
+            throw new Error(data);
         }
 
-        const data = await response.json();
-        const filteredData = data.results
-            .filter(item => {
-                return item.media_type !== 'person' && item.backdrop_path !== null;
-            })
-            .sort((a, b) => {
-                return b.popularity - a.popularity;
-            })
-            .map((item) => {
-                return {
-                    id: item.id,
-                    title: item.title || item.name,
-                    backdropPath: item.backdrop_path,
-                    type: item.media_type,
-                    releaseData: item.release_date || item.first_air_date,
-                    ratingAverage: item.vote_average,
-                    genreIds: item.genre_ids
-                }
-            });
-
-        return filteredData;
+        return data;
     } catch (error) {
         throw error;
     }
@@ -260,31 +151,15 @@ async function fetchSearchResults(searchQuery) {
  * @throws {Error} Throws an error if the API request fails or returns a non-OK status.
  */
 async function fetchMediaDetails(type, mediaId) {
-    const url = `https://api.themoviedb.org/3/${type}/${mediaId}?append_to_response=credits,recommendations`;
-
     try {
-        const response = await fetch(url, options);
-
-        if (!response.ok) {
-            throw new Error(`HTTP Error! Status: ${response.status}`);
-        }
-
+        const response = await fetch(`${endpoints.fetchMediaDetails}&type=${type}&mediaId=${mediaId}`);
         const data = await response.json();
 
-        return {
-            id: data.id,
-            title: data.title || data.name,
-            backdropPath: data.backdrop_path,
-            type: type,
-            releaseData: data.release_date || data.first_air_date,
-            runTime: data.runtime || data.number_of_seasons,
-            ratingAverage: data.vote_average,
-            genreIds: data.genres,
-            tagline: data.tagline,
-            cast: data.credits.cast,
-            recommendations: data.recommendations.results,
-            overview: data.overview
+        if (!response.ok) {
+            throw new Error(data);
         }
+
+        return data;
     } catch (error) {
         throw error;
     }
