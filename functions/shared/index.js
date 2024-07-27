377d3170dd8e9ef3e8a767d7ab145dc70291a929
tediko <tediko123@wp.pl>
2024-07-27 15:32:53 +0200

    Removed unnecessary base64toBlob

--- functions/shared/index.js
+++ functions/shared/index.js
@@ -2,28 +2,6 @@ import { Blob } from "buffer";
 const parser = require('lambda-multipart-parser');
 
 /**
- * Converts a Base64-encoded string into a Blob object.
- * @param {string} base64 - The Base64-encoded string
- * @returns {Blob} A Blob object representing the decoded data with the appropriate MIME type.
- */
-const base64ToBlob = (base64) => {
-    // Split the Base64 string into the metadata and the data
-    const [header, data] = base64.split(',');
-    // Extract the MIME type from the header
-    const mimeType = header.match(/:(.*?);/)[1];
-    // Decode the Base64 data
-    const byteString = atob(data);
-    // Create a byte array
-    const ab = new Uint8Array(byteString.length);
-    // Populate the byte array with byte values
-    for (let i = 0; i < byteString.length; i++) {
-        ab[i] = byteString.charCodeAt(i);
-    }
-    // Create a Blob from the byte array with the extracted MIME type
-    return new Blob([ab], { type: mimeType });
-}
-
-/**
  * Converts the downloaded Blob data to a Base64 data URL.
  * @param {Blob} data - The Blob object containing the downloaded file data.
  * @returns {Promise<string>} A promise that resolves to the Base64 data URL.
@@ -63,4 +41,4 @@ const parseAvatar = async (event) => {
     return { avatarFileName, avatarBlob };
 };
 
-export { base64ToBlob, blobToBase64DataUrl, parseAvatar }
+export { blobToBase64DataUrl, parseAvatar }
